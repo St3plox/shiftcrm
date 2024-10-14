703b52a9c720c2c8db8f9697b2b5f5defd60d6d1
St3plox <st3pegor@gmail.com>
2024-10-14 15:35:22 +0700

    Initial commit

---
+++ src/test/java/ru/tveu/shiftcrm/ShiftCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tveu.shiftcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
